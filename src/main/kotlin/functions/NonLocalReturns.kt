0f6cef6626968e66791b04c3c9036d6954c8dca9
Dandan Meng <mengdandanno1@163.com>
2019-07-10 16:43:35 +0800

    sample code for non-local return

---
+++ src/main/kotlin/functions/NonLocalReturns.kt
@@ -0,0 +1,22 @@
+package functions
+
+fun foo1() {
+    listOf(1, 2, 3, 4, 5).forEach {
+        if (it == 3) return // non-local return directly to the caller of foo()
+        print(it)
+    }
+    println("this point is unreachable")
+}
+
+fun foo2() {
+    listOf(1, 2, 3, 4, 5).forEach lit@{
+        if (it == 3) return@lit // local return to the caller of the lambda, i.e. the forEach loop
+        print(it)
+    }
+    print(" done with explicit label")
+}
+
+fun main() {
+//    foo1()
+    foo2()
+}
