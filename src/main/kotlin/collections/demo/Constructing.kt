7efa9f8697192b0c9058375fe20b5b2677e3d2af
Dandan Meng <mengdandanno1@163.com>
2019-10-12 10:27:47 +0800

    Collections demo: add copy examples

--- src/main/kotlin/collections/demo/Constructing.kt
+++ src/main/kotlin/collections/demo/Constructing.kt
@@ -4,17 +4,40 @@ fun main() {
     // set
     val numbersSet = setOf("one", "two", "three", "four")
     val numbersSetMutable = mutableSetOf("one", "two", "three", "four")
-    val emptySet = emptySet<String>()
 
     // list
     val numbersList = listOf("one", "two", "three", "four")
     val numbersListMutable = mutableListOf("one", "two", "three", "four")
-    val emptyList = emptyList<String>()
 
     // map
     val numbersMap = mapOf("key1" to 1, "key2" to 2, "key3" to 3, "key4" to 1)
     val numbersMapMutable = mutableMapOf("key1" to 1, "key2" to 2, "key3" to 3, "key4" to 1)
-    val emptyMap = emptyMap<String, Int>()
 
+    useEmptyCollections()
+
+    passReference()
+    copyList()
 }
 
+fun useEmptyCollections() {
+    val emptySet = emptySet<String>()
+    val emptyList = emptyList<String>()
+    val emptyMap = emptyMap<String, Int>()
+}
+
+fun passReference() {
+    val sourceList = mutableListOf(1, 2, 3)
+    val referenceList: List<Int> = sourceList
+//referenceList.add(4)            //compilation error
+    sourceList.add(4)
+    println(sourceList)
+    println(referenceList) // shows the current state of sourceList
+}
+
+fun copyList() {
+    val sourceList = mutableListOf(1, 2, 3)
+    val copyList = sourceList.toList()
+    sourceList.add(4)
+    println(sourceList)
+    println(copyList) // is different from current sourceList
+}
