564d96598cc265b6361f133a21c006efd070ff12
Dandan Meng <mengdandanno1@163.com>
2019-10-14 16:30:32 +0800

    add comparison to associateBy

--- src/main/kotlin/collections/demo/Grouping.kt
+++ src/main/kotlin/collections/demo/Grouping.kt
@@ -7,6 +7,10 @@ fun main() {
     val booksGroupByAuthor = getBooksGroupByAuthor(books)
     println(booksGroupByAuthor)
     println("Group size: ${booksGroupByAuthor.size}")
+
+    val associateByAuthor = books.associateBy { it.author }
+    println(associateByAuthor)
+    println("Associate size: ${associateByAuthor.size}")
 }
 
 fun getBooksGroupByAuthor(books: List<Book>): Map<String, List<Book>> {
