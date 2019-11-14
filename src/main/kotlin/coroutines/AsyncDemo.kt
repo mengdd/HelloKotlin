a15a4657f8821d91e2d78382c244a0c0a8aabc33
Dandan Meng <mengdandanno1@163.com>
2019-11-14 16:39:34 +0800

    add demo for async

---
+++ src/main/kotlin/coroutines/AsyncDemo.kt
@@ -0,0 +1,21 @@
+package coroutines
+
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    val deferred: Deferred<Int> = async {
+        loadData()
+    }
+    println("waiting...")
+    println(deferred.await())
+}
+
+suspend fun loadData(): Int {
+    println("loading...")
+    delay(1000L)
+    println("loaded!")
+    return 42
+}
