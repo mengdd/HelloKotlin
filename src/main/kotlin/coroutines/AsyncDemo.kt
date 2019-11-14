4d6d31dd5b64318bed0fc353e2b1b4d8686d1e69
Dandan Meng <mengdandanno1@163.com>
2019-11-14 17:03:06 +0800

    add some log info and comments

--- src/main/kotlin/coroutines/AsyncDemo.kt
+++ src/main/kotlin/coroutines/AsyncDemo.kt
@@ -6,16 +6,19 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
+    // @coroutine#1
+    println(Thread.currentThread().name)
     val deferred: Deferred<Int> = async {
+        // @coroutine#2
         loadData()
     }
-    println("waiting...")
-    println(deferred.await())
+    println("waiting..." + Thread.currentThread().name)
+    println(deferred.await()) // suspend @coroutine#1
 }
 
 suspend fun loadData(): Int {
-    println("loading...")
-    delay(1000L)
-    println("loaded!")
+    println("loading..." + Thread.currentThread().name)
+    delay(1000L) // suspend @coroutine#2
+    println("loaded!" + Thread.currentThread().name)
     return 42
 }
