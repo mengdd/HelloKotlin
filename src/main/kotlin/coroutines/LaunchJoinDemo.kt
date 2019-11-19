ec0f3f302b3fb526777103ea1061203435c14f55
Dandan Meng <mengdandanno1@163.com>
2019-11-19 16:51:45 +0800

    add demo for job.join() usage

---
+++ src/main/kotlin/coroutines/LaunchJoinDemo.kt
@@ -0,0 +1,16 @@
+package coroutines
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    val job = GlobalScope.launch {
+        // launch a new coroutine and keep a reference to its Job
+        delay(1000L)
+        println("World! + ${Thread.currentThread().name}")
+    }
+    println("Hello, + ${Thread.currentThread().name}")
+    job.join() // wait until child coroutine completes
+}
