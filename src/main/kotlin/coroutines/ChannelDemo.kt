624d8d001682c20c77835d4ba2a12089f315f544
Dandan Meng <mengdandanno1@163.com>
2019-11-27 15:05:25 +0800

    add demo for coroutines channels

---
+++ src/main/kotlin/coroutines/ChannelDemo.kt
@@ -0,0 +1,29 @@
+package coroutines
+
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+
+fun main() = runBlocking<Unit> {
+    val channel = Channel<String>()
+    launch {
+        channel.send("A1")
+        channel.send("A2")
+        log("A done")
+    }
+    launch {
+        channel.send("B1")
+        log("B done")
+    }
+    launch {
+        repeat(3) {
+            val x = channel.receive()
+            log(x)
+        }
+    }
+}
+
+fun log(message: Any?) {
+    println("[${Thread.currentThread().name}] $message")
+}
