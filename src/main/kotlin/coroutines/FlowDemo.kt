e96706be1af9958b3a6d7daeeddb755c125650bf
Dandan Meng <mengdandanno1@163.com>
2019-12-06 16:12:10 +0800

    add demo for flow

---
+++ src/main/kotlin/coroutines/FlowDemo.kt
@@ -0,0 +1,29 @@
+package coroutines
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+
+fun foo(): Flow<Int> = flow {
+    // flow builder
+    for (i in 1..3) {
+        delay(1000) // pretend we are doing something useful here
+        emit(i) // emit next value
+    }
+}
+
+fun main() = runBlocking<Unit> {
+    // Launch a concurrent coroutine to check if the main thread is blocked
+    launch {
+        for (k in 1..3) {
+            println("I'm not blocked $k")
+            delay(1000)
+        }
+    }
+    // Collect the flow
+    foo().collect { value -> println(value) }
+}
