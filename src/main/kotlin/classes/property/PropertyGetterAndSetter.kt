45e46e72d66b4dfa485a09850d6d9bbc83e57521
Dandan Meng <mengdandanno1@163.com>
2020-01-03 10:08:41 +0800

    add setter for property

--- src/main/kotlin/classes/property/PropertyGetterAndSetter.kt
+++ src/main/kotlin/classes/property/PropertyGetterAndSetter.kt
@@ -2,11 +2,21 @@ package classes.property
 
 fun main() {
     val car = Car()
+    car.name = ""
     car.speed = "50"
+    println(car.name)
     println(car.speed)
 }
 
 class Car {
     var speed: String = "0"
         get() = "$field km/h"
+
+    var name: String = "no name set"
+        get() = "the car's name is: $field"
+        set(value) {
+            if (value.isNotEmpty()) {
+                field = value
+            }
+        }
 }
